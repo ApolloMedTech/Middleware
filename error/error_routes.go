816717f678ad37dcd41929e38f17437e85c1361f
Alexandre Torres <alex_048@msn.com>
2023-11-18 15:06:46 +0000

    update

--- error/error_routes.go
+++ error/error_routes.go
@@ -1,19 +1,20 @@
 package error
 
 import (
+	"github.com/ApolloMedTech/Middleware/config"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func RegisterErrorRoutes(router *gin.Engine) {
+func RegisterErrorRoutes(router *gin.Engine, cfg config.Config) {
 	// Handle 404 Not Found
 	router.NoRoute(func(c *gin.Context) {
-		errorHandler(c, http.StatusNotFound, "Page not found")
+		errorHandler(c, http.StatusNotFound, "Page not found", cfg)
 	})
 
 	// Handle 405 Method Not Allowed
 	router.NoMethod(func(c *gin.Context) {
-		errorHandler(c, http.StatusMethodNotAllowed, "Method not allowed")
+		errorHandler(c, http.StatusMethodNotAllowed, "Method not allowed", cfg)
 	})
 
 	// You can add more error handlers here
