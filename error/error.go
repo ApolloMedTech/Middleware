6e34cd7bbfcdac4cd0e77e72de72caaf036bddb7
Alexandre Torres <alex_048@msn.com>
2023-11-18 14:45:55 +0000

    Update error.go

--- error/error.go
+++ error/error.go
@@ -2,6 +2,7 @@ package error
 
 import (
 	"fmt"
+	"github.com/ApolloMedTech/Middleware/config"
 	"github.com/ApolloMedTech/Middleware/http_template"
 	"github.com/flosch/pongo2/v6"
 	"github.com/gin-gonic/gin"
@@ -9,13 +10,13 @@ import (
 	"net/http"
 )
 
-func CustomErrorHandling() gin.HandlerFunc {
+func CustomErrorHandling(cfg config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if r := recover(); r != nil {
 				// Handle panic
 				logrus.Panic("Panic recovered: ", r)
-				errorHandler(c, http.StatusInternalServerError, fmt.Sprintf("%s", r))
+				errorHandler(c, http.StatusInternalServerError, fmt.Sprintf("%s", r), cfg)
 			}
 		}()
 		c.Next() // Process request
@@ -24,12 +25,12 @@ func CustomErrorHandling() gin.HandlerFunc {
 			// Handle the first error
 			err := c.Errors[0].Err
 			logrus.Error("Error: ", err.Error())
-			errorHandler(c, http.StatusInternalServerError, err.Error())
+			errorHandler(c, http.StatusInternalServerError, err.Error(), cfg)
 		}
 	}
 }
 
-func errorHandler(c *gin.Context, statusCode int, message string) {
+func errorHandler(c *gin.Context, statusCode int, message string, config config.Config) {
 	// Set the status code and render the template
-	http_template.Render(c, "templates/error.html", pongo2.Context{"status": statusCode, "message": message})
+	http_template.Render(c, config.TemplatesPath+"/error.html", pongo2.Context{"status": statusCode, "message": message})
 }
