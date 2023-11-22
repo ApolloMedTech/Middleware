8edef17da980c166db97cf94269c85baffbd3549
Alexandre Torres <alex_048@msn.com>
2023-11-22 22:21:42 +0000

    Update config_loader.go

--- config/config_loader.go
+++ config/config_loader.go
@@ -1,23 +1,34 @@
 package config
 
 import (
+	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 	"os"
+	"sync"
+)
+
+// global variable to store the configuration
+var (
+	config Config
+	once   sync.Once
 )
 
 // LoadConfig loads the configuration from a specified YAML file path.
-func LoadConfig(filePath string) (Config, error) {
-	var config Config
+func LoadConfig(filePath string) {
+	once.Do(func() {
+		yamlFile, err := os.ReadFile(filePath)
+		if err != nil {
+			logrus.Fatalf("Failed to read config file: %s", err)
+		}
 
-	yamlFile, err := os.ReadFile(filePath)
-	if err != nil {
-		return config, err
-	}
+		err = yaml.Unmarshal(yamlFile, &config)
+		if err != nil {
+			logrus.Fatalf("Failed to unmarshal config: %s", err)
+		}
+	})
+}
 
-	err = yaml.Unmarshal(yamlFile, &config)
-	if err != nil {
-		return config, err
-	}
-
-	return config, nil
+// GetConfig returns the global configuration
+func GetConfig() *Config {
+	return &config
 }
