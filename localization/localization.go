4ec6b080b4678fff77a656e8a42bc3349a26cde4
Alexandre Torres <alex_048@msn.com>
2023-11-29 12:00:15 +0000

    Update localization.go

--- localization/localization.go
+++ localization/localization.go
@@ -13,6 +13,7 @@ import (
 )
 
 var bundle *i18n.Bundle
+var trie *Trie // Declare trie as a global variable
 
 func InitLocalization(config config.LocalizationConfig) {
 	// Initialize the Bundle with the default language.
@@ -25,7 +26,7 @@ func InitLocalization(config config.LocalizationConfig) {
 		logrus.Error("Error loading locale files: ", err)
 		return
 	}
-
+	trie = newTrie() // Initialize the trie
 }
 
 func LoadLocaleFiles(path string) error {
@@ -109,4 +110,60 @@ func LocalizeDate(c *gin.Context, year int, month int, day int) string {
 		},
 	})
 	return dateString
+}
+
+// LocalizePrefixStrings Modify the  function to use SearchPrefix to find all keys that start with the given prefix, and then localize those strings.
+func LocalizePrefixStrings(localizer *i18n.Localizer, partialID string) map[string]string {
+	localizedStrings := make(map[string]string)
+	ids := trie.searchPrefix(partialID)
+	for _, id := range ids {
+		localizedStrings[id] = localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: id})
+	}
+	return localizedStrings
+}
+
+type TrieNode struct {
+	children map[rune]*TrieNode
+	endOfKey bool
+}
+
+type Trie struct {
+	root *TrieNode
+}
+
+func newTrie() *Trie {
+	return &Trie{root: &TrieNode{children: make(map[rune]*TrieNode)}}
+}
+
+func (t *Trie) insert(key string) {
+	node := t.root
+	for _, char := range key {
+		if _, ok := node.children[char]; !ok {
+			node.children[char] = &TrieNode{children: make(map[rune]*TrieNode)}
+		}
+		node = node.children[char]
+	}
+	node.endOfKey = true
+}
+
+func (t *Trie) searchPrefix(prefix string) []string {
+	node := t.root
+	for _, char := range prefix {
+		if _, ok := node.children[char]; !ok {
+			return nil
+		}
+		node = node.children[char]
+	}
+	var results []string
+	t.collectAllKeys(node, prefix, &results)
+	return results
+}
+
+func (t *Trie) collectAllKeys(node *TrieNode, prefix string, results *[]string) {
+	if node.endOfKey {
+		*results = append(*results, prefix)
+	}
+	for char, childNode := range node.children {
+		t.collectAllKeys(childNode, prefix+string(char), results)
+	}
 }
