c0fa2e17656c3c52aedf2e74ece50af772367886
Alexandre Monteiro <125086148+DarkMatterAlchemist@users.noreply.github.com>
2023-12-03 20:47:15 +0000

    Criei um request manager.

---
+++ requestManager/requestManager.go
@@ -0,0 +1,58 @@
+package requestmanager
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PerformGETHTTPRequest(c *gin.Context, url string, requestBody interface{}, responseData interface{}) error {
+	return performHTTPRequest(c, "GET", url, requestBody, responseData)
+}
+
+func PerformPOSTHTTPRequest(c *gin.Context, url string, requestBody interface{}, responseData interface{}) error {
+	return performHTTPRequest(c, "POST", url, requestBody, responseData)
+}
+
+func performHTTPRequest(c *gin.Context, method, url string, requestBody interface{}, responseData interface{}) error {
+	var body io.Reader
+
+	if requestBody != nil {
+		jsonData, err := json.Marshal(requestBody)
+		if err != nil {
+			return fmt.Errorf("failed to marshal request body: %v", err)
+		}
+		body = bytes.NewBuffer(jsonData)
+	}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errMsg := fmt.Sprintf("Microservice returned status: %d", resp.StatusCode)
+		return fmt.Errorf("status not OK: %s", errMsg)
+	}
+
+	if responseData != nil {
+		if err := json.NewDecoder(resp.Body).Decode(responseData); err != nil {
+			return fmt.Errorf("failed to decode response: %v", err)
+		}
+	}
+
+	return nil
+}
