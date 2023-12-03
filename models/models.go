125873429e0bd54c4e297b3bd641d795118ac180
Sr-Carneiro <72666995+Sr-Carneiro@users.noreply.github.com>
2023-12-03 00:41:31 +0000

    Add AccessControlRecord struct to models.go

--- models/models.go
+++ models/models.go
@@ -7,3 +7,9 @@ type MedicalRecord struct {
 	DateOfRecord          time.Time // The original timestamp
 	DateOfRecordFormatted string    // The formatted date as a string
 }
+
+type AccessControlRecord struct {
+	PersonnelName      string    // The name of the medical personnel who has access
+	StartDate          time.Time // The original timestamp
+	StartDateFormatted string    // The formatted date as a string
+}
