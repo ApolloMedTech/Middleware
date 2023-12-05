f384ada804025bdee3df6cb2d847463c2c8de87e
Alexandre Torres <alex_048@msn.com>
2023-12-05 09:47:25 +0000

    Update config_loader.go

--- config/config_loader.go
+++ config/config_loader.go
@@ -25,6 +25,26 @@ func LoadConfig(filePath string) {
 		if err != nil {
 			logrus.Fatalf("Failed to unmarshal config: %s", err)
 		}
+
+		// Override with environment variables if they are set
+		if port := os.Getenv("SERVER_PORT"); port != "" {
+			config.ServerConfig.Port = port
+		}
+		if user := os.Getenv("DB_USER"); user != "" {
+			config.Database.User = user
+		}
+		if password := os.Getenv("DB_PASSWORD"); password != "" {
+			config.Database.Password = password
+		}
+		if host := os.Getenv("DB_HOST"); host != "" {
+			config.Database.Host = host
+		}
+		if dbPort := os.Getenv("DB_PORT"); dbPort != "" {
+			config.Database.Port = dbPort
+		}
+		if dbName := os.Getenv("DB_NAME"); dbName != "" {
+			config.Database.Name = dbName
+		}
 	})
 }
 
