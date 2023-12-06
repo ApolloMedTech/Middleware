5edafd8eb4804735ad619dc6e9e3f4752b6823a2
Sr-Carneiro <72666995+Sr-Carneiro@users.noreply.github.com>
2023-12-06 01:07:08 +0000

    Fix typo in AccessStatus variable name

--- models/models.go
+++ models/models.go
@@ -12,5 +12,5 @@ type AccessControlRecord struct {
 	PersonnelName      string    // The name of the medical personnel who has access
 	StartDate          time.Time // The original timestamp
 	StartDateFormatted string    // The formatted date as a string
-	AcessStatus        string    // The access status
+	AccessStatus       string    // The access status
 }
