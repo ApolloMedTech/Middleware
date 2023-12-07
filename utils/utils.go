89c1d44b92780cb4f524b35f0b82a47535a2665e
mariapc01 <mpcao1@iscte-iul.pt>
2023-12-07 19:44:16 +0000

    func ValidateDate

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"errors"
 	"strconv"
+	"time"
 )
 
 // ValidateNumeroUtente checks if the numeroUtente string is valid based on specific rules.
@@ -20,6 +21,44 @@ func ValidateNumeroUtente(numeroUtente string) error {
 	// Check if all characters are numbers
 	if _, err := strconv.Atoi(numeroUtente); err != nil {
 		return errors.New("Numero utente must contain only numbers")
+	}
+
+	return nil
+}
+
+func ValidateDate(date string) error {
+	dateformat := "02/01/2006"
+	//check if date is empty
+	if date == "" {
+		return errors.New("Date parameter is empty")
+	}
+
+	//check if date is in correct format
+	parsedTime, err := time.Parse(dateformat, date)
+	if err != nil {
+		return errors.New("Date is not in correct format")
+	}
+
+	//check if date is after current date
+	currentTime := time.Now()
+	if parsedTime.Before(currentTime) || parsedTime.Equal(currentTime) {
+		return errors.New("Date is before current date")
+	}
+
+	//check if the days correspond to the month
+
+	// Extract the day from the parsed date
+	day := parsedTime.Day()
+
+	// Extract the year and month from the parsed date
+	year, month, _ := parsedTime.Date()
+
+	// Calculate the last day of the month
+	lastDayOfMonth := time.Date(year, month+1, 0, 0, 0, 0, 0, time.UTC).Day()
+
+	// Check if the day is valid for the chosen month
+	if day >= 1 && day <= lastDayOfMonth {
+		return errors.New("Date is out of range")
 	}
 
 	return nil
