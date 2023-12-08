59e573a83d05b3cb115a5021144e931b4566ae9e
mariapc01 <mpcao1@iscte-iul.pt>
2023-12-08 02:48:13 +0000

    debuggginggggg

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strconv"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 // ValidateNumeroUtente checks if the numeroUtente string is valid based on specific rules.
@@ -26,22 +28,26 @@ func ValidateNumeroUtente(numeroUtente string) error {
 	return nil
 }
 
+// ValidateDate checks if the date string is valid based on specific rules.
 func ValidateDate(date string) error {
 	dateformat := "02/01/2006"
 	//check if date is empty
 	if date == "" {
+		logrus.Debug("date is empty " + date)
 		return errors.New("Date parameter is empty")
 	}
 
 	//check if date is in correct format
 	parsedTime, err := time.Parse(dateformat, date)
 	if err != nil {
+		logrus.Debug("date is in correct format " + date + " parsedTime " + parsedTime.GoString())
 		return errors.New("Date is not in correct format")
 	}
 
 	//check if date is after current date
 	currentTime := time.Now()
 	if parsedTime.Before(currentTime) || parsedTime.Equal(currentTime) {
+		logrus.Debug("date is after current date " + currentTime.GoString() + " parsedTime " + parsedTime.GoString())
 		return errors.New("Date is before current date")
 	}
 
@@ -49,15 +55,18 @@ func ValidateDate(date string) error {
 
 	// Extract the day from the parsed date
 	day := parsedTime.Day()
-
+	logrus.Debug("days correspond to the month - day ")
+	logrus.Debug(day)
 	// Extract the year and month from the parsed date
 	year, month, _ := parsedTime.Date()
-
+	logrus.Debug(month)
+	logrus.Debug(year)
 	// Calculate the last day of the month
 	lastDayOfMonth := time.Date(year, month+1, 0, 0, 0, 0, 0, time.UTC).Day()
-
+	logrus.Debug(lastDayOfMonth)
 	// Check if the day is valid for the chosen month
 	if day >= 1 && day <= lastDayOfMonth {
+		logrus.Debug("in the if 69")
 		return errors.New("Date is out of range")
 	}
 
